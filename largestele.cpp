3decd5f1b3012823924e1c5b26a9631e0ed7ea39
Tanvi Santosh Valunj <valunjtanvi@gmail.com>
2026-05-14 01:43:18 +0530

    Add files via upload

---
+++ largestele.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[5]={1,2,3,4,5};
+    int largest=arr[0];
+    for(int i=0;i<5;i++)
+    {
+        if(arr[i]>largest)
+        {
+            largest=arr[i];
+        }
+    }
+    cout<<"Largest element is "<<largest;
+    return 0;
+}
